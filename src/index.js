05daebe9754dc8cf5f611622d441119cfc22175d
sahilnayak <snayak.researcher@gmail.com>
2016-11-30 00:12:10 -0800

    Need to add some styling, but basic functionality is there

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ class App extends Component {
     }
 
     render() {
-        const videoSearch = _.debounce((term)=>{this.videoSearch(item)}, 300);
+        const videoSearch = _.debounce((term)=>{this.videoSearch(term)}, 300);
         return (<div>
                     <SearchBar onSearchTermChange={videoSearch}/>
                     <VideoDetail video={this.state.selectedVideo}/>
